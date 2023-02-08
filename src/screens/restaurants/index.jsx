a0355410b537e72aa37494362413ebf9d114d7ce
Leennguyen <nguyenthilinh26112002@gmail.com>
2023-02-08 11:43:36 +0700

    exprot things

---
+++ src/screens/restaurants/index.jsx
@@ -0,0 +1,5 @@
+export {RestaurantLayout} from './RestaurantLayout'
+export {RestaurantNewsletter} from './RestaurantNewsletter'
+export {RestaurantMenu} from './RestaurantMenu'
+export {RestaurantGallery} from './RestaurantGallery'
+export { RestaurantInfo } from './RestaurantInfo'
