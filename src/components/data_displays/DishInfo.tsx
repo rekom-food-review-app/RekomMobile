ef07b86bafbc7001525d3cc9a45d1168550b5a12
Nguyenthilinh_2611 <nguyenthilinh26112002@gmail.com>
2023-03-02 15:38:44 +0700

    feat: navigate to FoodDetail when click on name of food on ResMenu.

--- src/components/data_displays/DishInfo.tsx
+++ src/components/data_displays/DishInfo.tsx
@@ -1,18 +1,20 @@
 import React from "react";
-import {Image, StyleSheet, View} from "react-native";
+import {Image, StyleSheet, TouchableOpacity, View} from "react-native";
 import {CsText} from "./CsText";
 import {DishInfoApiType} from "../../@types/DishInfoApiType";
+import { useNavigation } from "@react-navigation/native";
 
 interface DishInfoProps extends DishInfoApiType {
    wrapperStyle?: any
 }
 
 const DishInfo = (props: DishInfoProps) => {
+   const nav = useNavigation<any>()
    return (
       <View style={[defaultStyle.contain, props.wrapperStyle]}>
          <Image source={{uri: props.imageUrl}} style={defaultStyle.img}/>
          <View style={{paddingLeft: 1}}>
-            <CsText weight={"bold"} style={{marginTop: 8}} color={'A'} size={'md'}>{props.name}</CsText>
+            <TouchableOpacity onPress={() => nav.navigate('Food')}><CsText weight={"bold"} style={{marginTop: 8}} color={'A'} size={'md'}>{props.name}</CsText></TouchableOpacity>
             <CsText weight={900} style={{marginTop: 5}} size={'md'}>{props.price}K</CsText>
          </View>
       </View>
